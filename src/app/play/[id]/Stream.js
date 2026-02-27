969dd0f1d23cc8d72a5b430b74c9fb5b3114aef3
saksham vitwekar <saksham.vitwekar@gmail.com>
2026-02-27 23:22:50 +0530

    v1.0 completed

---
+++ src/app/play/[id]/Stream.js
@@ -0,0 +1,34 @@
+"use client"
+import Hls from "hls.js";
+import { useEffect, useRef } from "react";
+
+export default function Stream({url}){
+    const videoRef = useRef(null);
+    useEffect(() => {
+        const video = videoRef.current;
+        if (!video) return;
+
+        if (Hls.isSupported()) {
+            const hls = new Hls({liveSyncDurationCount: 3});
+            hls.loadSource(url);
+            hls.attachMedia(video);
+            } 
+            else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+            video.src = url;
+            } 
+            else {
+            console.log("HLS not supported");
+            }
+    }, [url]);
+    return(
+        <>
+        <div className="min-h-screen bg-black flex items-center justify-center">
+            <video
+                ref={videoRef}
+                controls
+                className="w-full max-w-4xl rounded-lg"
+            />
+        </div>
+        </>
+    );
+}
---
+++ src/app/play/[id]/page.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import Stream from "./Stream";
+
+
+export default async function play({ params }){
+    const {id} = await params;
+    const streams = await (await axios.get("https://iptv-org.github.io/api/streams.json")).data;
+    const stream = streams.find((e) => e.channel == id);
+    const channelStreams = streams.filter(
+        e => e.channel?.toLowerCase() === id.toLowerCase()
+    );
+    console.log(
+        channelStreams
+    );
+    return(
+        <>
+            {typeof stream !== 'undefined' ? <Stream url={stream.url}/> : <h1> Not allowed under copyright laws</h1>}
+        </>
+    );
+}
