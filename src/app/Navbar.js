728a68d0ef1c0a3c26952deaeb26dc1e9e859702
saksham vitwekar <saksham.vitwekar@gmail.com>
2026-02-27 23:41:18 +0530

    Updating logo

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,5 +1,5 @@
 import { Bebas_Neue } from 'next/font/google'
-
+import Link from 'next/link';
 const bebas = Bebas_Neue({
   subsets: ['latin'],
   weight: '400',
@@ -8,7 +8,9 @@ const bebas = Bebas_Neue({
 export default function Navbar(){
     return(
         <nav className='flex items-center px-6 py-4 bg-black text-white'>
-            <h1 className={`text-blue-700 ${bebas.className} text-3xl`}>StreamWave TV</h1>
+            <Link href={"/"}>
+                <h1 className={`text-blue-700 ${bebas.className} text-3xl`}>StreamWave TV</h1>
+            </Link>
             <div className="flex gap-6 mx-auto"/>
             <button className="border border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white px-5 py-2 rounded-lg transition duration-300">
                 Change Region
